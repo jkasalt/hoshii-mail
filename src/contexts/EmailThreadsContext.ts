6d704333753d99cad8a7561e141d45e613e70eb1
Luca Bracone <lucabr123@gmail.com>
2025-01-01 09:49:15 +0100

    rm unused contexts

--- src/contexts/EmailThreadsContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, type Dispatch } from "react";
-import type { EmailThread } from "../types/EmailThread";
-import type { EmailThreadAction } from "../reducers/EmailThreadReducer";
-
-export const EmailThreadsContext = createContext<EmailThread[] | null>(null);
-export const EmailThreadsDispatchContext =
-  createContext<Dispatch<EmailThreadAction> | null>(null);
--- src/contexts/SelectedReply.ts
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const SelectedReply = createContext<number | null>(null);
